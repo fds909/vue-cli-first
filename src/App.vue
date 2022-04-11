9d3d317e7c9eaf45f0d2bf842a1f50cb774de730
Francesco De Sìmini <francescod995@gmail.com>
2022-04-11 14:59:18 +0200

    creato e usato componente TitleComp

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TitleComp></TitleComp>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleComp from './components/TitleComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TitleComp
   }
 }
 </script>
---
+++ src/components/TitleComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>La mia prima app con Vue CLI!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TitleComp',
+}
+</script>
+
+<style>
+    h2 {
+        color: rgb(102, 26, 26);
+    }
+</style>
